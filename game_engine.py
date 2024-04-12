699f1c2568ff0e536990593e8fdbb4bd7561b683
Stanislav Glushkov <4eu3ep13@mail.ru>
2024-04-13 02:37:27 +0400

    display class applied to game engine

--- game_engine.py
+++ game_engine.py
@@ -2,51 +2,21 @@ import datetime
 import json
 import sys
 
+from display import Display
+
 default_config = "default_config.json"
 default_preset = "default_preset.json"
 
 
-def validate_preset(config, preset):
-    if not preset["weeks_passed"] in range(config["weeks_may_pass"][0], config["weeks_may_pass"][1] + 1):
-        print("Weeks_passed value from preset don't pass config requirements.")
-        sys.exit(0)
-
-    if not preset["day"] in range(config["days_in_week"][0], config["days_in_week"][1] + 1):
-        print("weeks_passed value from preset don't pass config requirements.")
-        sys.exit(0)
-
-    if not preset["starting_phase"] in config["phases_available"]:
-        print("Game phase from preset is not in available phases in config.")
-        sys.exit(0)
-
-    if not preset["situation"] in config["situations_available"]:
-        print("Game situation from preset is not in available situations in config.")
-        sys.exit(0)
-
-    if not preset["current_influence"] in range(config["influence_range"][0], config["influence_range"][1] + 1):
-        print("current_influence value from preset don't pass config requirements.")
-        sys.exit(0)
-
-    if not preset["map"] in config["maps"]:
-        print("Map from preset is not in available phases in config.")
-        sys.exit(0)
-
-    for effect in preset["active_effects"]:
-        if effect not in config["available_effects"]:
-            print(effect, "from preset active effects is not in available effects in config.")
-            sys.exit(0)
-
-    if not preset["players"] in config["player_collections"]:
-        print("Game characters from preset is not in available collections in config.")
-        sys.exit(0)
-
-
 class GameEngine:
 
-    def __init__(self, config_file_path, game_preset_file_path):
+    def __init__(self, config_file_path, game_preset_file_path, lang):
+
+        self.show = Display(lang)
+        self.show.phrase("welcome")
 
         start_time = datetime.datetime.now()
-        print("Game engine started at", start_time)
+        self.show.engine_startup_with_timestamp("start", start_time)
         self.weeks_passed = "undefined"
         self.day = "undefined"
         self.phase = "undefined"
@@ -58,7 +28,8 @@ class GameEngine:
 
         GameEngine.use_config_and_preset(self, config_file_path, game_preset_file_path)
 
-        print("Game engine ready at", start_time)
+        end_time = datetime.datetime.now()
+        self.show.engine_startup_with_timestamp("end", end_time)
 
     def play(self):
         result = ""
@@ -76,67 +47,52 @@ class GameEngine:
             elif self.phase == "sunrise":
                 self.play_sunrise()
 
-        print(result)
-        print("Game ended")
+        self.show.phrase(result)
+        self.show.phrase("the_end")
 
     def check_game_result(self):
-        result = "No result"
+        result = "no_result"
         if self.current_influence >= 13:
-            result = "Dracula wins!"
+            result = "dracula_win"
             self.phase = "end"
 
         for player in self.players:
             if player["class"] == "dracula":
                 if player["dynamic"]["wounds"] >= player["max_wounds"]:
-                    result = "Hunters win!"
+                    result = "hunters_win"
                     self.phase = "end"
         return result
 
     def prepare_game(self):
-        print("Game preparation in progress")
+        self.show.phrase("prepare_game")
         hunters_spawn_available = [location for location in self.map if location["type"] == "city"]
 
         hunters_indexes = [i for i in range(len(self.players)) if self.players[i]["class"] != "dracula"]
         dracula_index = [index for index in range(len(self.players)) if index not in hunters_indexes][0]
 
-        confirms = [False] * len(hunters_indexes)
+        confirms = [False]
 
-        print()
-        print("Current hunters position:")
-        for index in hunters_indexes:
-            print(self.players[index]["name_ru"], ": ", self.players[index]["dynamic"]["location"], sep="")
+        self.show.current_hunters_position(self.players, hunters_indexes)
 
         while not any(confirms):
-            print()
-            print("Available start locations:")
-            for city in hunters_spawn_available:
-                if city["big"]:
-                    size = "big"
-                else:
-                    size = "small"
-
-                print(city["title_ru"], ", size: ", size, sep="")
-            print()
+            self.show.available_start_locations(hunters_spawn_available)
 
             dracula_available_spawn_locations = [location["id"] for location in hunters_spawn_available]
             for index in hunters_indexes:
                 location = "undefined"
                 while location not in [str(location["id"]) for location in self.map]:
-                    print(self.players[index]["name_ru"], ", please, specify spawn location NUMBER from available:",
-                          sep="")
+                    self.show.ask_player_to_choose_start_location(self.players[index])
                     location = input()
                 if int(location) in dracula_available_spawn_locations:
                     dracula_available_spawn_locations.remove(int(location))
                 self.players[index]["dynamic"]["location"] = location
 
-            print("Current hunters position:")
-            for index in hunters_indexes:
-                print(self.players[index]["name_ru"], ": ", self.players[index]["dynamic"]["location"], sep="",
-                      end=", ")
+            self.show.current_hunters_position(self.players, hunters_indexes)
 
             confirms = []
             for index in hunters_indexes:
-                print(self.players[index]["name_ru"], "please, confirm readiness by typing Y below:")
+                self.show.ask_hunter_to_confirm_readiness(self.players[index])
+                # print(self.players[index]["name_ru"], "please, confirm readiness by typing Y below:")
                 response = input()
                 if response.upper() == "Y":
                     confirms.append(True)
@@ -144,14 +100,10 @@ class GameEngine:
                     confirms.append(False)
                     break
 
-        print()
-        print("Final hunters position:")
-        for index in hunters_indexes:
-            print(self.players[index]["name_ru"], ": ", self.players[index]["dynamic"]["location"], sep="",
-                  )
+        self.show.current_hunters_position(self.players, hunters_indexes, final=True)
 
         while (self.players[dracula_index]["dynamic"]["location"]) not in dracula_available_spawn_locations:
-            print("Dracula, please, specify NUMBER of your starting position:")
+            self.show.ask_player_to_choose_start_location(self.players[dracula_index])
             location = input()
             if location.isdigit():
                 self.players[dracula_index]["dynamic"]["location"] = int(location)
@@ -159,35 +111,35 @@ class GameEngine:
         self.phase = "day"
 
     def play_day(self):
-        print("Play day")
+        self.show.phrase("play_day")
         self.phase = "sunset"
         return
 
     def play_sunset(self):
-        print('Play sunset')
+        self.show.phrase("play_sunset")
         self.phase = "night"
         return
 
     def play_night(self):
-        print("Play night")
+        self.show.phrase("play_night")
         self.phase = "sunrise"
         return
 
     def play_sunrise(self):
-        print("Play sunrise")
+        self.show.phrase("play_sunrise")
         self.phase = "end"
         return
 
     def use_config_and_preset(self, config_file_path, game_preset_file_path):
 
         if not config_file_path:
-            print("Engine uses default config")
+            self.show.phrase("default_config")
             config = default_config
         else:
             config = config_file_path
 
         if not game_preset_file_path:
-            print("Engine uses default preset")
+            self.show.phrase("default_preset")
             preset = default_preset
         else:
             preset = game_preset_file_path
@@ -197,7 +149,7 @@ class GameEngine:
         with open(preset) as preset:
             game_preset = json.load(preset)
 
-        validate_preset(game_config, game_preset)
+        self.validate_preset(game_config, game_preset)
 
         self.weeks_passed = game_preset["weeks_passed"]
         self.day = game_preset["day"]
@@ -209,5 +161,39 @@ class GameEngine:
             self.map = json.load(map_file)
         with open(game_preset["players"]) as players:
             self.players = json.load(players)
+
+    def validate_preset(self, config, preset):
+        if not preset["weeks_passed"] in range(config["weeks_may_pass"][0], config["weeks_may_pass"][1] + 1):
+            self.show.phrase("weeks_value_error")
+            sys.exit(0)
+
+        if not preset["day"] in range(config["days_in_week"][0], config["days_in_week"][1] + 1):
+            self.show.phrase("day_value_error")
+            sys.exit(0)
+
+        if not preset["starting_phase"] in config["phases_available"]:
+            self.show.phrase("phase_error")
+            sys.exit(0)
+
+        if not preset["situation"] in config["situations_available"]:
+            self.show.phrase("situation_error")
+            sys.exit(0)
+
+        if not preset["current_influence"] in range(config["influence_range"][0], config["influence_range"][1] + 1):
+            self.show.phrase("influence_error")
+            sys.exit(0)
+
+        if not preset["map"] in config["maps"]:
+            self.show.phrase("map_error")
+            sys.exit(0)
+
+        for effect in preset["active_effects"]:
+            if effect not in config["available_effects"]:
+                self.show.effect_error(effect)
+                sys.exit(0)
+
+        if not preset["players"] in config["player_collections"]:
+            self.show.phrase("character_error")
+            sys.exit(0)
 
 #  TODO: implement classes for: map, character
