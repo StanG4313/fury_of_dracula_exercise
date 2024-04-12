b0eb2b36f8f45e327aedba16060736cef235e5fa
Stanislav Glushkov <4eu3ep13@mail.ru>
2024-04-13 02:39:08 +0400

    display class applied to start.py

--- start.py
+++ start.py
@@ -3,15 +3,15 @@ import sys
 from game_engine import GameEngine
 
 
-def run_main_game_process(config_file_path=None, game_preset_path=None):
+def run_main_game_process(config_file_path=None, game_preset_path=None, lang="EN"):
 
-    print("Welcome to Fury of Dracula game!")
-    engine = GameEngine(config_file_path, game_preset_path)
+    engine = GameEngine(config_file_path, game_preset_path, lang)
     engine.play()
 
 
 if __name__ == "__main__":
     config_file = ""
     game_preset_file = ""
-    run_main_game_process(config_file, game_preset_file)
+    language = "EN"
+    run_main_game_process(config_file, game_preset_file, language)
     sys.exit(0)
